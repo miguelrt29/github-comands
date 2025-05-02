2f0b34bf6b6bbd5b074f5ffb078d45733a558ed7
miguelrt29 <miguelrt2903@gmail.com>
2025-05-02 14:17:47 -0500

    CAMBIOS en codigo

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+
+/* 
+Ejercicio 1: Fast Forward Merge
+Objetivo: Realizar un merge sin conflictos cuando la rama principal no tiene cambios nuevos.
+Instrucciones:
+1. Crea un nuevo repositorio en Git.
+2. Agrega un archivo Javascript con un mensaje en main().
+3. Crea y cambia a una nueva rama (feature-ff).
+4. Modifica el archivo Javascript en la nueva rama agregando un método adicional.
+5. Realiza un commit en esta rama.
+6. Vuelve a main y fusiona la rama con git merge feature-ff.
+7. Observa cómo Git hace un Fast Forward sin generar un commit de merge.
+*/
+
 function main (nombre){
     console.log(`Bienvenido a mi programa $ {nombre} ` );
 }
@@ -16,15 +30,19 @@ saludarEstudiante("VankVersity")
 saludarProfesores ("German")
 
 
-/* 
-Ejercicio 1: Fast Forward Merge
-Objetivo: Realizar un merge sin conflictos cuando la rama principal no tiene cambios nuevos.
+
+/* Ejercicio 2: Merge con cambios divergentes (sin conflictos)
+Objetivo: Realizar un merge cuando ambas ramas han cambiado diferentes líneas del archivo sin generar conflictos.
 Instrucciones:
-1. Crea un nuevo repositorio en Git.
-2. Agrega un archivo Javascript con un mensaje en main().
-3. Crea y cambia a una nueva rama (feature-ff).
-4. Modifica el archivo Javascript en la nueva rama agregando un método adicional.
-5. Realiza un commit en esta rama.
-6. Vuelve a main y fusiona la rama con git merge feature-ff.
-7. Observa cómo Git hace un Fast Forward sin generar un commit de merge.
-*/
+1. Asegúrate de estar en main y crea una nueva rama (feature-no-conflict).
+2. Modifica el archivo Javascript agregando una nueva línea de código.
+3. Realiza un commit en la rama.
+4. Cambia a main y modifica el mismo archivo, pero en una línea distinta.
+5. Realiza un commit en main.
+6. Intenta fusionar ambas ramas y verifica que Git lo hace automáticamente sin conflictos. */
+
+function cambios(cambios) {
+    console.log(`${cambios}`);
+  }
+  
+
